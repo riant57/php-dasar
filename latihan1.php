2e9b95fb1c0aa33317819561be9aed486c2615c5
Riantri cahyono <riant57@gmail.com>
2025-01-12 17:55:35 +0700

    Menambahkan percabangan if

--- latihan1.php
+++ latihan1.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 1</title>
+    <style>
+        .warna-baris{
+            background-color:silver;
+        }
+    </style>
 </head>
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
-        <?php for($x=1; $x < 5; $x++) :?>
-            <tr>
-                <?php for($j=1; $j < 5; $j++): ?>
-                    <td>1<?php echo $i.",".$j ?></td>
+        <?php for($x=1; $x <= 5; $x++) :?>
+            <?php if($x % 2 == 1): ?>
+            <tr class="warna-baris">
+            <?php else : ?>
+                <tr>
+            <?php endif ?>
+                <?php for($j=1; $j <= 5; $j++): ?>
+                    <td><?php echo $x.",".$j ?></td>
                 <?php endfor; ?>
             </tr>
         <?php  endfor;?>
