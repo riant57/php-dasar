0d73073affdb3987e6d6d91fc18dc6d51d1b6341
Riantri cahyono <riant57@gmail.com>
2025-01-12 17:44:05 +0700

    Menambahkan perulangan for

--- latihan1.php
+++ latihan1.php
@@ -6,6 +6,15 @@
     <title>Latihan 1</title>
 </head>
 <body>
-    
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php for($x=1; $x < 5; $x++) :?>
+            <tr>
+                <?php for($j=1; $j < 5; $j++): ?>
+                    <td>1<?php echo $i.",".$j ?></td>
+                <?php endfor; ?>
+            </tr>
+        <?php  endfor;?>
+    </table>
+
 </body>
 </html>
