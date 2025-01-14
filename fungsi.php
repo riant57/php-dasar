5142edf84efaedbe1719d1b1482e637fa76206dc
Rian tri cahyono <riant57@gmail.com>
2025-01-15 05:37:09 +0700

    Menambahkan file fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,17 @@
+<?php
+function salam(){
+    return "Salam";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Fungsi built in dan user defined</title>
+</head>
+<body>
+    <h1><?php echo salam() ?></h1>
+</body>
+</html>
